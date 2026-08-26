abdddf619453edd9535636ec58d37cf3d6c92695
Juan Carlos <jclacruzlacruz@icloud.com>
2026-08-26 13:19:06 +0200

    Remove the icon pipes that are no longer used
    ColorIcon resolves the default icon and the pickers show the label
    directly, so neither IconOrDefaultPipe nor IconLabelPipe has any use left.

--- src/app/shared/icons/icon-label.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { ICON_LABELS, IconName } from './icons';
-
-@Pipe({ name: 'iconLabel' })
-export class IconLabelPipe implements PipeTransform {
-  transform(icon: IconName): string {
-    return ICON_LABELS[icon];
-  }
-}
